e8150da16d08e6dd7508322467f54f6e8846b084
Periodical <typhfun@outlook.com>
2025-02-05 07:45:30 -0500

    Update Factor.jl

--- Factor.jl
+++ Factor.jl
@@ -1,14 +1,9 @@
 #!/usr/bin/julia
-text = "6x^2 -22x -84"
+# text = "6x^2 -22x -84"
 
-# Should be able to factor out completely (including the greatest common factors)
-# Should be able to factor out greatest common factors that are variables
-# Should have an output of "prime" or "not factorable" for prime expressions
-# Should be able to factor polynomials with leading coefficient that is not 1. 
+#text = "6x^2 -22x -84"
+text = "15a^2 + 45a + 60"
 
-#Tokenize Check
-#Combine like terms Check
-#Factor
 
 function Tokenize(text)
     if !startswith(text, ['+', '-'])
@@ -23,7 +18,6 @@ function Tokenize(text)
     for token in tokens
         TokensGC[i] = Dict()
         TokensGC[i]["token"] = token
-
         TokensGC[i]["variables"] = Dict()
         j = 1
         while j <= length(token)
@@ -49,18 +43,11 @@ function Tokenize(text)
                 j += 1
             end
         end
-
-        # Handle coefficients at the start of the token, including sign
-        if length(token) > 1
-            # Match the optional sign and digits at the start of the token
-            match_result = match(r"^([+-]?\d+)", token)
         
-            if match_result !== nothing
-                coefficient_str = match_result.captures[1]
-                TokensGC[i]["coefficient"] = parse(Int, coefficient_str)
-            else
-                TokensGC[i]["coefficient"] = 1  # Default coefficient is 1
-            end
+        match_result = match(r"^([+-]?\d+)", token)
+        if match_result !== nothing
+            coefficient_str = match_result.captures[1]
+            TokensGC[i]["coefficient"] = parse(Int, coefficient_str)
         else
             TokensGC[i]["coefficient"] = 1  # Default coefficient is 1
         end
@@ -104,81 +91,171 @@ function GreatestCommonFactor(coefficients)
     return gcf
 end
 
-function Factor(tokens)
-    coefficients = [tokens[key]["coefficient"] for key in keys(tokens)]
-    GCF = GreatestCommonFactor(coefficients)
+function FactorExpression(CLT)
+    coefficients = [term["coefficient"] for term in values(CLT)]
+    gcf = GreatestCommonFactor(coefficients)
     
-    if GCF == 1
-        error("Unfactorable")
+    factored_terms = Dict()
+    i = 1
+    for key in keys(CLT)
+        new_coeff = div(CLT[key]["coefficient"], gcf)
+        factored_terms[i] = Dict("coefficient" => new_coeff, "variables" => CLT[key]["variables"])
+        i+=1
     end
     
-    factored_tokens = Dict()
-    for key in keys(tokens)
-        token = tokens[key]
-        new_coefficient = token["coefficient"] ÷ GCF
-        new_key = string(new_coefficient)
-        for var in keys(token["variables"])
-            new_key *= var * "^" * string(token["variables"][var]["exponent"])
-        end
-        factored_tokens[new_key] = Dict("coefficient" => new_coefficient, "variables" => token["variables"])
-    end
-    
-    return GCF, factored_tokens
+    return gcf, factored_terms
 end
 
-function Quadratic_Roots(a, b, c)
-    Δ = b^2 - 4a*c
-    if Δ < 0
-        error("Unfactorable")
-    end
-    root1 = (-b + sqrt(Δ)) / (2a)
-    root2 = (-b - sqrt(Δ)) / (2a)
-    return root1, root2
-end
+# function FindFactorPair(factored_terms)
+#     a = parse(Int, match(r"-?\d+", factored_terms[1]).match)
+#     b = parse(Int, match(r"-?\d+", factored_terms[2]).match)
+#     c = parse(Int, match(r"-?\d+", factored_terms[3]).match)
+#     ac = a * c
+#     for i in 1:abs(ac)
+#         if ac % i == 0
+#             j = ac ÷ i
+#             if i + j == b
+#                 return i, j
+#             elseif i - j == b
+#                 return i, -j
+#             elseif -i + j == b
+#                 return -i, j
+#             elseif -i - j == b
+#                 return -i, -j
+#             end
+#         end
+#     end
+#     error("No valid factor pair found")
+# end
 
-function Format(GCF, factored_tokens)
-    terms = []
-    for key in keys(factored_tokens)
-        token = factored_tokens[key]
-        if length(token["variables"]) == 1 && haskey(token["variables"], "x") && token["variables"]["x"]["exponent"] == 2
-            a = token["coefficient"]
-            b = 0
-            c = 0
-            for var in keys(token["variables"])
-                if token["variables"][var]["exponent"] == 1
-                    b = token["coefficient"]
-                elseif token["variables"][var]["exponent"] == 0
-                    c = token["coefficient"]
-                end
-            end
-            root1, root2 = quadratic_roots(a, b, c)
-            term = string("(", a, "x + ", root1, ")", "(", "x + ", root2, ")")
-        else
-            term = string(token["coefficient"])
-            for var in keys(token["variables"])
-                if token["variables"][var]["exponent"] == 1
-                    term *= var
-                else
-                    term *= var * "^" * string(token["variables"][var]["exponent"])
+function FindFactorPair(factored_terms)
+    if length(factored_terms) == 3
+        a = factored_terms[1]["coefficient"]
+        b = factored_terms[2]["coefficient"]
+        c = factored_terms[3]["coefficient"]
+        ac = a * c
+        for i in 1:abs(ac)
+            if ac % i == 0
+                j = ac ÷ i
+                if i + j == b
+                    return i, j
+                elseif i - j == b
+                    return i, -j
+                elseif -i + j == b
+                    return -i, j
+                elseif -i - j == b
+                    return -i, -j
                 end
             end
         end
-        push!(terms, term)
+    elseif length(factored_terms) == 2
+        a = factored_terms[1]["coefficient"]
+        b = 0
+        c = factored_terms[2]["coefficient"]
+        ac = a * c
+        for i in 1:abs(ac)
+            if ac % i == 0
+                j = ac ÷ i
+                if i + j == b
+                    return i, j
+                elseif i - j == b
+                    return i, -j
+                elseif -i + j == b
+                    return -i, j
+                elseif -i - j == b
+                    return -i, -j
+                end
+            end
+        end
     end
-    return string(GCF, "(", join(terms, " + "), ")")
 end
+
+function input()
+    println("Enter the expression to be factored: ")
+    text = readline()
+    return text
+end
+
+function FactorEquation(factored_terms ,i, j, gcf)
+    a = factored_terms[1]["coefficient"]
+
+    ifa = GreatestCommonDivisor(i, a)
+    jfa = GreatestCommonDivisor(j, a)
+
+    i = string(div(i, ifa))
+    j = string(div(j, jfa))
+
+    a1 = div(a, ifa)
+    a2 = div(a, jfa)
+
+    if !startswith(i, ['+', '-'])
+        i = "+" * i
+    end
+    if !startswith(j, ['+', '-'])
+        j = "+" * j
+    end
+
+    if (gcf == 1)
+        gcf = ""
+    end
+    if (a1 == 1)
+        a1 = ""
+    end
+    if (a2 == 1)
+        a2 = ""
+    end
+
+    a1 = string(a1)
+    a2 = string(a2)
+    
+    a = gcf * "(" * a1 * "x " * i * ")(" * a2 * "x " * j * ")"
+    return filter(x -> !isspace(x), a)
+end
+
+text = input()
 
 TokensGC = Tokenize(text)
 CLT = CombineLikeTerms(TokensGC)
-
-#println("Combined Like Terms: ", CLT)
-
+gcf, factored_terms = FactorExpression(CLT)
 try
-    GCF, factored_tokens = Factor(CLT)
-    #println("GCF: ", GCF)
-    #println("Factored Tokens: ", factored_tokens)
-    Formatted_Result = Format(GCF, factored_tokens)
-    println("Formatted Result: ", Formatted_Result)
+    i, j = FindFactorPair(factored_terms)
+    finals = FactorEquation(factored_terms, i, j, gcf)
+    println("Factored Expression: ", finals)
 catch e
-    println(e)
 end
+if gcf == 1
+    gcf = ""
+end
+global first = true
+global finals = string(gcf) * "("
+for token in factored_terms
+    token_value = token[2]  # Access the value part of the Pair
+    if token_value["coefficient"] == 1
+        token_value["coefficient"] = ""
+    end
+    token_value["coefficient"] = string(token_value["coefficient"])
+    if !startswith(token_value["coefficient"], ['+', '-']) && !first
+        token_value["coefficient"] = "+" * token_value["coefficient"]
+    else 
+        global first = false
+    end
+    global finals *= string(token_value["coefficient"])
+    for var in values(token_value["variables"])
+        global finals *= var["variable"]
+        if var["exponent"] != 1
+            global finals *= "^" * string(var["exponent"])
+        end
+    end
+end
+global finals *= ")"
+println("Factored Expression: ", finals)
+
+# if length(factored_terms) == 3
+#     a = parse(Int, match(r"-?\d+", factored_terms[1]).match)
+#     factor1, factor2 = FindFactorPair(factored_terms)
+#     i2 = GreatestCommonDivisor(factor1, a)
+#     j2 = GreatestCommonDivisor(factor2, a)
+#     println("Factored Expression: ", gcf, "(", i2, "x + ", div(factor1, i2), ")(", j2, "x + ", div(factor2, j2), ")")
+# else
+#     println("Not a quadratic trinomial, skipping factor pair check.")
+# end
